20fb1cfbc7c641ea23f0838a723dfa07df7a9e4e
soo941226 <rhrnakrnakr@naver.com>
2021-06-20 21:00:25 +0900

    Feat: initialize the project

---
+++ MyRecorder/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyRecorder
+//
+//  Created by kjs on 2021/06/20.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func onClickRecordButton(_ sender: UIButton){
+        
+    }
+}
+
